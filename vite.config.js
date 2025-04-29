518b2806a6e2a6b7d8fb6a15c9fe6f83e046f0ca
vpvnguyen <vpvnguyen@gmail.com>
2025-04-28 20:51:44 -0700

    feat: include types definition

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,23 @@
 import { defineConfig } from 'vite';
 import path from 'path';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
+  plugins: [dts({
+    insertTypesEntry: true, // adds "types" to package.json exports
+  })],
   build: {
     lib: {
-      entry: path.resolve(__dirname, 'src/index.js'), // Your main entry file
+      entry: path.resolve(__dirname, 'src/index.ts'), // Your main entry file
       name: 'index', // UMD name (optional unless using UMD)
       fileName: (format) => `index.${format}.js`, // Output filenames
       formats: ['es', 'cjs'], // or ['es', 'umd'] if you want UMD
     },
     rollupOptions: {
-      external: ['react', 'vue'], // external dependencies (don't bundle them)
+      external: ['react'], // external dependencies (don't bundle them)
       output: {
         globals: {
           react: 'React',
-          vue: 'Vue'
         }
       }
     },
